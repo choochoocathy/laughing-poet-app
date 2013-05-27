911f373958201d1cef8d40e8d78bfef895a17ba3
Wayne <wayne@gmail.com>
2013-05-27 09:19:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poetapp::Application.config.secret_token = 'b36d6e9bdc8b7d82857da2b55d44f4d35fa773a8edf241ba401cc1e498d92d7712a20bfa8682148d71dedcdc7619e7bd3d9b4d4e7e669d3864d9ba9898fa3ba3'
